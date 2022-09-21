81862b825afdfe44cba0339d04b48c7bd5dfb506
Credusco <aliessonani@gmail.com>
2022-09-21 11:47:46 -0700

    7-leet

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,9 @@
+
+
+
+
+
+
 #ifndef MAIN_H
 #define MAIN_H
 #include<stdio.h>
@@ -9,7 +15,7 @@
 void reserve_array(int *a,intn);
 void print_number(int n);
 void print_buffer(char *b, int size);
-int _strcmp(char *s1,char *s2);
+int_strcmp(char *s1,char *s2);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -18,6 +24,6 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
-char *add_strings(char *n1,char *n2, char *r,int_index);
+char *add_string(char *n1,char *n2, char *r,int_index);
 
 #endif/*MAIN_H*/
